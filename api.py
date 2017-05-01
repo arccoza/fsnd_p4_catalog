5d7d8b1c9e7c8a3db9aae220cb98c7755358e391
Adrien <adrien@arccoza.com>
2017-05-01 14:32:27 +0900

    Modify resources and models to return better json

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, Response
 from flask_restful import Resource, Api, reqparse
 from pony.orm import show, select, db_session, Database, PrimaryKey, Required,\
     Optional, Set
@@ -15,6 +15,13 @@ db = Database()
 class User(db.Entity):
     username = Required(str)
 
+    def to_dict(self):
+        return {key: attr.__get__(self) for key, attr in self._adict_.items()}
+        # return {attr.name: val for attr, val in self._vals_.items()}
+
+    def to_json(self):
+        return json.dumps(self.to_dict())
+
 
 class Category(db.Entity):
     created = Required(datetime, sql_default='CURRENT_TIMESTAMP')
@@ -30,10 +37,6 @@ class Item(db.Entity):
     categories = Set('Category', reverse='items')
 
 
-# with db.permissions_for(UserModel, CatagoryModel, ItemModel):
-#     allow('view', group='anybody')
-
-
 class UserRes(Resource):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -45,8 +48,9 @@ class UserRes(Resource):
         with db_session:
             users = select(u for u in User)[:]
 
-        print(to_dict(users[0]))
-        return to_json(users)
+        print(users[0].to_dict())
+        return [u.to_dict() for u in users]
+        # return Response((bytes(u.to_json(), 'utf8') for u in users), mimetype='application/json')
 
     def post(self):
         kwargs = self.input.parse_args()
@@ -55,7 +59,7 @@ class UserRes(Resource):
         with db_session:
             user = User(**kwargs)
 
-        return to_json(user), 201
+        return user.to_dict(), 201
 
 
 api.add_resource(UserRes, '/users/', '/users/<int:id>')
