0cf45beabc4bf2590a39ec1a5a8d3e0ba3f8ab9f
Adrien <adrien@arccoza.com>
2017-05-01 12:33:41 +0900

    Add db creation, if not exist

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 import locallib
 from flask import Flask
 from api import api_bp, db
+import psycopg2 as psql
 
 
 app = Flask(__name__)
 app.register_blueprint(api_bp, url_prefix='/api')
+
+# Ref: http://stackoverflow.com/questions/34484066/create-a-postgres-database-using-python
+# Create the required database if it doesn't exist.
+con = psql.connect(dbname='postgres')
+con.autocommit = True
+cur = con.cursor()
+try:
+    cur.execute('CREATE DATABASE catalog;')
+except:
+    pass
+
 db.bind('postgres', database='catalog')
 db.generate_mapping(create_tables=True)
 
