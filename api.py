4fa0604fc71de3bab6e44e87eca58f753edbe943
Adrien <adrien@arccoza.com>
2017-05-01 12:34:55 +0900

    Add models and get + post for user resource

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
 from flask import Blueprint
-from flask_restful import Resource, Api
-from pony.orm import show, select, db_session, Database, PrimaryKey, Required, Set
+from flask_restful import Resource, Api, reqparse
+from pony.orm import show, select, db_session, Database, PrimaryKey, Required,\
+    Optional, Set
+from pony.orm.serialization import to_json, to_dict
+import json
 
 
 api_bp = Blueprint('api', __name__)
@@ -12,11 +15,42 @@ class UserModel(db.Entity):
     username = Required(str)
 
 
+class CatagoryModel(db.Entity):
+    title = Required(str)
+    # description = Optional(str)
+
+
+class ItemModel(db.Entity):
+    title = Required(str)
+    # description = Optional(str)
+
+
+# with db.permissions_for(UserModel, CatagoryModel, ItemModel):
+#     allow('view', group='anybody')
+
+
 class UserRes(Resource):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        input = reqparse.RequestParser()
+        input.add_argument('username', type=str, help='User\'s handle.')
+        self.input = input
+
     def get(self, id=None):
         with db_session:
-            return select(u for u in UserModel)[:]
-        # return {'name': 'Dean Venture'}
+            users = select(u for u in UserModel)[:]
+
+        print(to_dict(users[0]))
+        return to_json(users)
+
+    def post(self):
+        kwargs = self.input.parse_args()
+        user = None
+
+        with db_session:
+            user = UserModel(**kwargs)
+
+        return to_json(user), 201
 
 
 api.add_resource(UserRes, '/users/', '/users/<int:id>')
