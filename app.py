b8520757ee6841f82b198512f7adb1ec32e853b6
Adrien <adrien@arccoza.com>
2017-05-10 13:23:53 +0900

    Add custom session interface experiment

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import locallib
 from flask import Flask, render_template, url_for, session
+from flask.sessions import SecureCookieSessionInterface
 from api import api_bp
 from models import db
 import psycopg2 as psql
 
+
+class CookieSessionInterface(SecureCookieSessionInterface):
+    def save_session(self, app, session, response):
+        if session:
+            super().save_session(app, session, response)
+        else:
+            response.set_cookie(app.session_cookie_name, '', expires=0)
+        print('$$$$$$$$$$$$$$$$$', session)
+
+
 secret_key = 'taU>5&(Z*+r2d5ULR|i2z$bt@+ 9|i,;u!N_2);v@i/@y;gUf/&0WXC?}g6<aK$H'
 app = Flask(__name__, static_folder='pub')
+app.session_interface = CookieSessionInterface()
 app.secret_key = secret_key
 app.register_blueprint(api_bp, url_prefix='/api')
 
