7c28247deee0eddc13440082885f635fc4d7c196
Adrien <adrien@arccoza.com>
2017-05-17 17:19:17 +0900

    Update flyfile build script to generate single browser js file

--- flyfile.js
+++ flyfile.js
@@ -1,25 +1,25 @@
-const fly = require('fly-esnext')
+const Fly = require("fly")
 const babel = require('rollup-plugin-babel')
 const nodeResolve = require('rollup-plugin-node-resolve')
 const commonjs = require('rollup-plugin-commonjs')
 const json = require('rollup-plugin-json')
-const svelte = require('rollup-plugin-svelte')
+// const svelte = require('rollup-plugin-svelte')
 const print = console.log.bind(console)
 
 
 const src = {
-  js: 'src/js/index.js',
-  css: 'src/css/index.css'
+  js: 'src/**/js/index.js',
+  css: 'src/**/css/index.css'
 }
 
 const dest = 'pub'
 
-exports.clean = async function(fly) {
-    await fly.clear([dest]);
+exports.clean = function*(fly) {
+    yield fly.clear([dest]);
 }
 
-exports.js = async function(fly) {
-  await fly.source(src.js).rollup({
+exports.js = function*(fly) {
+  yield fly.source(src.js).rollup({
     rollup: {
       plugins: [
         nodeResolve({ jsnext: true, main: true, browser: true, preferBuiltins: false }),
@@ -31,22 +31,22 @@ exports.js = async function(fly) {
             '*.json'
           ],
         }),
-        svelte({
-          // By default, all .html and .svelte files are compiled
-          // extensions: [ '.my-custom-extension' ],
+        // svelte({
+        //   // By default, all .html and .svelte files are compiled
+        //   // extensions: [ '.my-custom-extension' ],
 
-          // You can restrict which files are compiled
-          // using `include` and `exclude`
-          include: 'src/components/**/*.html',
+        //   // You can restrict which files are compiled
+        //   // using `include` and `exclude`
+        //   include: 'src/components/**/*.html',
 
-          // By default, the client-side compiler is used. You
-          // can also use the server-side rendering compiler
-          generate: 'ssr',
+        //   // By default, the client-side compiler is used. You
+        //   // can also use the server-side rendering compiler
+        //   generate: 'ssr',
 
-          // If you're doing server-side rendering, you may want
-          // to prevent the client-side compiler from duplicating CSS
-          css: false
-        }),
+        //   // If you're doing server-side rendering, you may want
+        //   // to prevent the client-side compiler from duplicating CSS
+        //   css: false
+        // }),
       ]
     },
     bundle: {
@@ -56,3 +56,13 @@ exports.js = async function(fly) {
     }
   }).target(dest);
 }
+
+exports.default = async function(fly) {
+  await fly.serial(['js'])
+}
+
+// const fly = new Fly(module.exports)
+
+// fly.start('js')
+// .then(print)
+// .catch(print)
