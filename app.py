b90b8db695a6364ff2918f9b4c1f0830037acefd
Adrien <adrien@arccoza.com>
2017-05-17 17:19:56 +0900

    Update flask init to point static url to pub folder

--- app.py
+++ app.py
@@ -6,18 +6,18 @@ from models import db
 import psycopg2 as psql
 
 
-class CookieSessionInterface(SecureCookieSessionInterface):
-    def save_session(self, app, session, response):
-        if session:
-            super().save_session(app, session, response)
-        else:
-            response.set_cookie(app.session_cookie_name, '', expires=0)
-        print('$$$$$$$$$$$$$$$$$', session)
+# class CookieSessionInterface(SecureCookieSessionInterface):
+#     def save_session(self, app, session, response):
+#         if session:
+#             super().save_session(app, session, response)
+#         else:
+#             response.set_cookie(app.session_cookie_name, '', expires=0)
+#         print('$$$$$$$$$$$$$$$$$', session)
 
 
 secret_key = 'taU>5&(Z*+r2d5ULR|i2z$bt@+ 9|i,;u!N_2);v@i/@y;gUf/&0WXC?}g6<aK$H'
-app = Flask(__name__, static_folder='pub')
-app.session_interface = CookieSessionInterface()
+app = Flask(__name__, static_folder='pub', static_url_path='/static')
+# app.session_interface = CookieSessionInterface()
 app.secret_key = secret_key
 app.register_blueprint(api_bp, url_prefix='/api')
 
