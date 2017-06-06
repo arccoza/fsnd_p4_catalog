1f11c0cde3cc8122f24a90107c54ff7259e617dc
Adrien <adrien@arccoza.com>
2017-06-06 16:28:22 +0900

    Move AppHeader component into its own file

---
+++ src/js/react-app-header.js
@@ -0,0 +1,187 @@
+import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import {lightTheme, darkTheme} from './react-themes'
+import AppBar from 'material-ui/AppBar'
+import FlatButton from 'material-ui/FlatButton'
+import RaisedButton from 'material-ui/RaisedButton'
+import Dialog from 'material-ui/Dialog'
+import IconButton from 'material-ui/IconButton'
+import FontIcon from 'material-ui/FontIcon'
+import CircularProgress from 'material-ui/CircularProgress'
+import Avatar from 'material-ui/Avatar'
+import NavigationClose from 'material-ui/svg-icons/navigation/close'
+import Auth from './auth'
+
+
+const Theme = (props) => (
+  <MuiThemeProvider muiTheme={getMuiTheme(props.theme)}>
+    {props.children}
+  </MuiThemeProvider>
+)
+
+const vlayout = {
+  display: 'flex',
+  flexFlow: 'column wrap',
+  justifyContent: 'space-between',
+  alignContent: 'center',
+  alignItems: 'stretch',
+}
+
+const hlayout = {
+  display: 'flex',
+  flexFlow: 'row wrap',
+  justifyContent: 'space-between',
+  alignContent: 'center',
+  alignItems: 'stretch',
+}
+
+const layoutStack = {
+  position: 'relative',
+  display: 'flex',
+  // width: '100%',
+  flexFlow: 'column nowrap',
+  justifyContent: 'space-between',
+  alignContent: 'center',
+  alignItems: 'stretch',
+}
+
+function merge(...objs) {
+  return Object.assign({}, ...objs)
+}
+
+export default class AppHeader extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      // isAuthed: false,
+      isDialogOpen: false,
+      isBusy: false,
+      provider: null,
+    }
+  }
+
+  _signin = (provider) => {
+    this.setState({isBusy: true, provider: authProviders[provider]})
+
+    authProviders[provider]
+      .signin()
+      .then(resp => {
+        this._closeDialog()
+        this.setState({isBusy: false})
+        this.props.pub('message', {content: 'Welcome...', action: null})
+        this.props.pub('user', resp.user)
+      })
+      .catch(err => {
+        this._closeDialog()
+        this.setState({isBusy: false})
+        this.props.pub('message', {content: 'Signin failed.', action: null})
+        this.props.pub('user', null)
+      })
+  }
+
+   _signout = (provider) => {
+    this.state.provider
+      .signout()
+      .then(resp => {
+        this.props.pub('message', {content: 'Goodbye...', action: null})
+        this.props.pub('user', null)
+      })
+      .catch(err => {
+        this.props.pub('message', {content: 'Signout failed.', action: null})
+      })
+   }
+
+  _openDialog = () => {
+    this.setState({isDialogOpen: true})
+  }
+
+  _closeDialog = () => {
+    this.setState({isDialogOpen: false})
+  }
+
+  render() {
+    if(!this.state.isBusy) {
+      var dialogContent = (
+        <div style={merge(hlayout, {justifyContent: 'space-around'})}>
+          <RaisedButton
+            name='facebook'
+            onTouchTap={ev => this._signin('facebook')}
+            label='Facebook'
+            labelPosition='after'
+            primary={true}
+            icon={<FontIcon className='socicon-facebook' />}
+            buttonStyle={{backgroundColor: '#3e5b98'}}
+          />
+          <RaisedButton
+            name='google'
+            onTouchTap={ev => this._signin('google')}
+            label='Google'
+            labelPosition='after'
+            primary={true}
+            icon={<FontIcon className='socicon-google' />}
+            buttonStyle={{backgroundColor: '#dd4b39'}}
+          />
+        </div>
+      )
+    }
+    else {
+      var dialogContent = (
+        <div style={merge(hlayout, {justifyContent: 'space-around'})}>
+          <CircularProgress />
+        </div>
+      )
+    }
+
+    if(!this.props.user) {
+      var appBar = (
+        <div style={layoutStack}>
+          <AppBar style={{
+              backgroundColor: 'transparent',
+              borderBottom: '1px solid white',
+              borderBottomColor: lightTheme.palette.borderColor,
+            }}
+            title="Catalog"
+            iconElementRight={<FlatButton label='Signin' onTouchTap={this._openDialog}/>}
+            onLeftIconButtonTouchTap={ev => this.props.pub('nav', {isOpen: true})}
+          />
+          <Dialog
+            title='Signin with...'
+            actions={[
+                <FlatButton label='Cancel' onTouchTap={this._closeDialog} />
+              ]}
+            modal={false}
+            open={this.state.isDialogOpen}
+            onRequestClose={this._closeDialog}
+          >
+            {dialogContent}
+          </Dialog>
+        </div>
+      )
+    }
+    else {
+      var appBar = (
+        <div style={layoutStack}>
+          <AppBar style={{
+              backgroundColor: 'transparent',
+              borderBottom: '1px solid white',
+              borderBottomColor: lightTheme.palette.borderColor,
+            }}
+            title="Catalog"
+            onLeftIconButtonTouchTap={ev => this.props.pub('nav', {isOpen: true})}
+            iconElementRight={
+              <FlatButton label={this.props.user.name} onTouchTap={this._signout} style={hlayout}>
+                <Theme theme={darkTheme}>
+                  <NavigationClose style={{verticalAlign: 'middle'}} />
+                </Theme>
+                <Avatar src={this.props.user.picture} size={28} style={{verticalAlign: 'middle'}} />
+              </FlatButton>
+            }
+          />
+        </div>
+      )
+    }
+
+    return appBar
+  }
+}
