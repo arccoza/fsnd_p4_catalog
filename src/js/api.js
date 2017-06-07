5a1b45a96d8e76e1da048dfa9af6ef9a735b4165
Adrien <adrien@arccoza.com>
2017-06-07 23:14:51 +0900

    Add api.js file to consolidate fetching data from the REST API

---
+++ src/js/api.js
@@ -0,0 +1,29 @@
+function apiRequest(method, what, id, data) {
+  var input = `/api/${what}/${Array.isArray(id) ? id.join(',') : id || ''}`
+  var init = {
+    method: method,
+    credentials: 'include',
+    headers: {
+      'X-Requested-With': 'Fetch',
+    },
+  }
+
+  if(data)
+    init.body = JSON.stringify(data)
+
+  return fetch(input, init)
+  .then(resp => Promise.all([resp, resp.json()]))
+  .then(([resp, json]) => {
+    if(!resp.ok)
+      throw json
+    return json
+  })
+}
+
+var api = {
+  get: apiRequest.bind(null, 'get'),
+  set: apiRequest.bind(null, 'put'),
+  // mod: apiRequest.bind(null, 'patch'),
+  add: apiRequest.bind(null, 'post'),
+  rem: apiRequest.bind(null, 'delete'),
+}
