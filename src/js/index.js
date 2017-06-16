052f435a3e7d59dc4540d6a4c8112522ac3efe14
Adrien <adrien@arccoza.com>
2017-06-17 00:51:34 +0900

    Test setup

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect,
+  withRouter
+} from 'react-router-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import RaisedButton from 'material-ui/RaisedButton';
+import App from './react-app.js'
 import Auth from './auth.js'
 var print = console.log.bind(console)
 
 
+injectTapEventPlugin();
 var googleScriptSrc = '//apis.google.com/js/platform.js'
 var facebookScriptSrc = '//connect.facebook.net/en_US/sdk.js'
 
@@ -21,3 +35,88 @@ var fb = new Auth({provider: 'facebook', scriptSrc:facebookScriptSrc, cred:{
 
 window.gg = gg
 window.fb = fb
+
+
+window.addEventListener('load', ev => {
+
+  // function App() {
+  //   return (
+  //     <MuiThemeProvider>
+  //       <RaisedButton label="Route A" />
+  //       <RaisedButton label="Route B" />
+  //     </MuiThemeProvider>
+  //   )
+  // }
+
+  const Home = () => (
+    <div>
+      <h2>Home</h2>
+    </div>
+  )
+
+  const About = () => (
+    <div>
+      <h2>About</h2>
+    </div>
+  )
+
+  const Topic = ({ match }) => (
+    <div>
+      <h3>{match.params.topicId}</h3>
+    </div>
+  )
+
+  const Topics = ({ match }) => (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${match.url}/rendering`}>
+            Rendering with React
+          </Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/components`}>
+            Components
+          </Link>
+        </li>
+        <li>
+          <Link to={`${match.url}/props-v-state`}>
+            Props v. State
+          </Link>
+        </li>
+      </ul>
+
+      <Route path={`${match.url}/:topicId`} component={Topic}/>
+      <Route exact path={match.url} render={() => (
+        <h3>Please select a topic.</h3>
+      )}/>
+    </div>
+  )
+
+  const BasicExample = () => (
+    <Router>
+      <div>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/topics">Topics</Link></li>
+        </ul>
+
+        <hr/>
+
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/topics" component={Topics}/>
+      </div>
+    </Router>
+  )
+
+
+
+  ReactDOM.render(
+    <Router><App /></Router>,
+    document.getElementById('root')
+  )
+
+})
