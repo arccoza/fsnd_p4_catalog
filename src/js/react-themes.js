d1930173ac96b47a59af6982746a779fcc57564e
Adrien <adrien@arccoza.com>
2017-08-24 11:32:08 +0900

    Move `Theme` component into `react-themes.js`

--- src/js/react-themes.js
+++ src/js/react-themes.js
@@ -1,50 +1,52 @@
 import {
-  cyan700,
   grey600,
   pinkA100, pinkA200, pinkA400,
   redA400,
   purple900, deepPurple700,
-  teal500, teal300, teal100,
+  cyan500, cyan300, cyan100,
   lime500,
   fullWhite, white,
   fullBlack, black,
 } from 'material-ui/styles/colors'
 import {fade, darken, lighten} from 'material-ui/utils/colorManipulator'
 import spacing from 'material-ui/styles/spacing'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import React from 'react'
 
 
 var lightPalette = {
-  primary1Color: teal500,
-  primary2Color: teal300,
-  primary3Color: teal100,
-  accent1Color: teal300,
-  accent2Color: teal300,
-  accent3Color: teal100,
-  textColor: teal500,
-  secondaryTextColor: teal500,
+  primary1Color: cyan500,
+  primary2Color: cyan300,
+  primary3Color: cyan100,
+  accent1Color: cyan300,
+  accent2Color: cyan300,
+  accent3Color: cyan100,
+  textColor: cyan500,
+  secondaryTextColor: cyan500,
   alternateTextColor: white,
   canvasColor: white,
-  borderColor: teal300,
+  borderColor: cyan300,
   disabledColor: fade(fullBlack, 0.3),
-  pickerHeaderColor: teal300,
+  pickerHeaderColor: cyan300,
   clockCircleColor: white,
 }
 
 var darkPalette = {
   primary1Color: white,
-  primary2Color: teal100,
-  primary3Color: teal300,
-  accent1Color: teal100,
-  accent2Color: teal100,
-  accent3Color: teal300,
+  primary2Color: cyan100,
+  primary3Color: cyan300,
+  accent1Color: cyan100,
+  accent2Color: cyan100,
+  accent3Color: cyan300,
   textColor: white,
   secondaryTextColor: white,
-  alternateTextColor: teal500,
-  canvasColor: teal500,
-  borderColor: teal300,
+  alternateTextColor: cyan500,
+  canvasColor: cyan500,
+  borderColor: cyan300,
   disabledColor: fade(fullBlack, 0.3),
   pickerHeaderColor: white,
-  clockCircleColor: teal500,
+  clockCircleColor: cyan500,
 }
 
 var themeGen = (palette) => {
@@ -98,4 +100,10 @@ var themeGen = (palette) => {
 const lightTheme = themeGen(lightPalette)
 const darkTheme = themeGen(darkPalette)
 
-export {lightTheme, darkTheme}
+const Theme = (props) => (
+  <MuiThemeProvider muiTheme={getMuiTheme(props.theme)}>
+    {props.children}
+  </MuiThemeProvider>
+)
+
+export {lightTheme, darkTheme, Theme}
