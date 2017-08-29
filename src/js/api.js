57eb59cb0ae3404d779004e23258fb5d610eb15d
Adrien <adrien@arccoza.com>
2017-08-29 15:53:08 +0900

    Move json encoder into separate fn `prepJson`

--- src/js/api.js
+++ src/js/api.js
@@ -7,19 +7,11 @@ function tmpl(str, args) {
   })
 }
 
-function apiRequest(method, what, id, data) {
-  var input = `/api/${what}/${Array.isArray(id) ? id.join(',') : id || ''}`
-  var init = {
-    method: method,
-    credentials: 'include',
-    headers: {
-      'X-Requested-With': 'Fetch',
-      'Content-Type': 'application/json',
-    },
-  }
-  var start = Promise.resolve()
+function prepJson(init, data) {
   var promises = []
   var blobs = []
+
+  init['Content-Type'] = 'application/json'
 
   if(data) {
     init.body = JSON.stringify(data, (k, v) => {
@@ -47,8 +39,22 @@ function apiRequest(method, what, id, data) {
   }
 
   if (promises.length) {
-    start = Promise.all(promises).then(out => init.body = tmpl(init.body, out))
+    return Promise.all(promises).then(out => init.body = tmpl(init.body, out))
   }
+  else
+    return Promise.resolve()
+}
+
+function apiRequest(method, what, id, data) {
+  var input = `/api/${what}/${Array.isArray(id) ? id.join(',') : id || ''}`
+  var init = {
+    method: method,
+    credentials: 'include',
+    headers: {
+      'X-Requested-With': 'Fetch',
+    },
+  }
+  var start = prepJson(init, data)
 
   return start.then(resp => fetch(input, init))
   .then(resp => Promise.all([resp, resp.json()]))
