821b57bc191f126c53276b66eaed849440ce1ed8
Adrien <adrien@arccoza.com>
2017-08-31 14:12:49 +0900

    Add icons file to hold all used material icons

---
+++ src/js/icons.js
@@ -0,0 +1,15 @@
+import StarBorder from 'material-ui/svg-icons/toggle/star-border'
+import NavigationClose from 'material-ui/svg-icons/navigation/close'
+import NavigationChevronRight from 'material-ui/svg-icons/navigation/chevron-right'
+import ContentAdd from 'material-ui/svg-icons/content/add'
+import ContentAddBox from 'material-ui/svg-icons/content/add-box'
+import AvLibraryAdd from 'material-ui/svg-icons/av/library-add'
+
+export {
+  StarBorder,
+  NavigationClose,
+  NavigationChevronRight,
+  ContentAdd,
+  ContentAddBox,
+  AvLibraryAdd,
+}
