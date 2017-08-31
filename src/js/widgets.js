66ab379572876f42cf0c6c045bf815852d2b3c2f
Adrien <adrien@arccoza.com>
2017-08-31 13:37:01 +0900

    Add widgets file to hold used material widgets
    Much easier for importing into other files than from the
    material package directly, because the material package requires
    you to import each widget from a seperate file.

---
+++ src/js/widgets.js
@@ -0,0 +1,51 @@
+import AppBar from 'material-ui/AppBar'
+import {GridList, GridTile} from 'material-ui/GridList'
+import {List, ListItem} from 'material-ui/List'
+import Subheader from 'material-ui/Subheader'
+import Divider from 'material-ui/Divider'
+import TextField from 'material-ui/TextField'
+import SelectField from 'material-ui/SelectField'
+import DropDownMenu from 'material-ui/DropDownMenu'
+import Menu from 'material-ui/Menu'
+import MenuItem from 'material-ui/MenuItem'
+import Popover from 'material-ui/Popover'
+import Toggle from 'material-ui/Toggle'
+import Paper from 'material-ui/Paper'
+import Dialog from 'material-ui/Dialog'
+import Snackbar from 'material-ui/Snackbar'
+import Drawer from 'material-ui/Drawer'
+import FlatButton from 'material-ui/FlatButton'
+import RaisedButton from 'material-ui/RaisedButton'
+import IconButton from 'material-ui/IconButton'
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import FontIcon from 'material-ui/FontIcon'
+import CircularProgress from 'material-ui/CircularProgress'
+import Avatar from 'material-ui/Avatar'
+
+export {
+  AppBar,
+  GridList,
+  GridTile,
+  List,
+  ListItem,
+  Subheader,
+  Divider,
+  TextField,
+  SelectField,
+  DropDownMenu,
+  Menu,
+  MenuItem,
+  Popover,
+  Toggle,
+  Paper,
+  Dialog,
+  Snackbar,
+  Drawer,
+  FlatButton,
+  RaisedButton,
+  IconButton,
+  FloatingActionButton,
+  FontIcon,
+  CircularProgress,
+  Avatar,
+}
