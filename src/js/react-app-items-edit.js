1d3ef2d1427bac00235aed9be060cc70daf31461
Adrien <adrien@arccoza.com>
2017-09-05 17:24:24 +0900

    Change form layout

--- src/js/react-app-items-edit.js
+++ src/js/react-app-items-edit.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {lightTheme, darkTheme, Theme} from './react-themes'
-import {TextField, SelectField, MenuItem, Paper, RaisedButton, Chip} from './widgets'
+import {TextField, SelectField, MenuItem, Paper, RaisedButton, FlatButton, Chip} from './widgets'
 import {layout} from './utils'
 import api from './api.js'
 var h = React.createElement
@@ -10,55 +10,14 @@ var print = console.log.bind(console)
 export function EditItem({categories, curItem, curImage, setField, modify}) {
   var fileInput
 
+  var cats = curItem.categories.map((v, i) => h(Chip, {key: v,
+    onRequestDelete: ev => modify(null, 'curItem', 'categories', i),
+    style: {margin: '0.25em'},
+  }, (categories.find(e => e.id === v) || {}).title ))
+
   var col1 =
   h('div', {style: layout({dr: 'v', 'jc': '<'})},
     h('h2', null, 'Edit Item'),
-    h(TextField, {
-      hintText: 'ex: Snowboard',
-      floatingLabelText: 'Enter item name',
-      value: curItem.title,
-      onChange: setField('curItem', 'title'),
-    }),
-    h(TextField, {
-      hintText: 'ex: An awesome snowboard',
-      floatingLabelText: 'Enter item description',
-      value: curItem.description,
-      onChange: setField('curItem', 'description'),
-    }),
-    h(SelectField, {
-      floatingLabelText: 'Select a category',
-      value: curItem.categories,
-      listStyle: {backgroundColor: '#fff'},
-      menuItemStyle: {color: '#00bcd4'},
-      multiple: true,
-      onChange: (ev, k, v) => modify(v, 'curItem', 'categories'),
-    },
-      categories.map(cat => (
-          h(MenuItem, {key: cat.id, value: cat.id, primaryText: cat.title})
-        )
-      )
-    ),
-    h(TextField, {
-      hintText: 'ex: Hank Venture',
-      floatingLabelText: 'The author of this item',
-      value: curItem.author,
-    }),
-    h(TextField, {
-      hintText: 'ex: item.png',
-      floatingLabelText: 'An image of the item',
-      value: curImage.name,
-      onTouchTap: ev => fileInput.click()
-    }),
-    h('input', {
-      ref: el => fileInput = el,
-      type: 'file',
-      accept: 'image/*',
-      style: {display: 'none'},
-      onChange: ({target:{files:[file]}}) => {
-        if (file)
-          modify({name: file.name, type: file.type, blob: file}, 'curImage')
-      },
-    }),
     h(RaisedButton, {
       label: 'Save',
       primary: true,
@@ -86,22 +45,68 @@ export function EditItem({categories, curItem, curImage, setField, modify}) {
         .then(([data]) => print(data))
       }
     }),
+    h(FlatButton, {
+      label: 'Delete',
+      primary: false,
+      style: {margin: '1em 0 0 0'},
+    }),
+    h(TextField, {
+      hintText: 'ex: Snowboard',
+      floatingLabelText: 'Enter item name',
+      value: curItem.title,
+      onChange: setField('curItem', 'title'),
+    }),
+    h(TextField, {
+      hintText: 'ex: An awesome snowboard',
+      floatingLabelText: 'Enter item description',
+      value: curItem.description,
+      onChange: setField('curItem', 'description'),
+    }),
+    h(TextField, {
+      hintText: 'ex: Hank Venture',
+      floatingLabelText: 'The author of this item',
+      value: curItem.author,
+    }),
+    h(TextField, {
+      hintText: 'ex: item.png',
+      floatingLabelText: 'An image of the item',
+      value: curImage.name,
+      onTouchTap: ev => fileInput.click()
+    }),
+    h('input', {
+      ref: el => fileInput = el,
+      type: 'file',
+      accept: 'image/*',
+      style: {display: 'none'},
+      onChange: ({target:{files:[file]}}) => {
+        if (file)
+          modify({name: file.name, type: file.type, blob: file}, 'curImage')
+      },
+    }),
+    h(SelectField, {
+      floatingLabelText: 'Select category',
+      value: curItem.categories,
+      listStyle: {backgroundColor: '#fff'},
+      menuItemStyle: {color: '#00bcd4'},
+      multiple: true,
+      onChange: (ev, k, v) => modify(v, 'curItem', 'categories'),
+    },
+      categories.map(cat => (
+          h(MenuItem, {key: cat.id, value: cat.id, primaryText: cat.title})
+        )
+      )
+    ),
+    h('div', {style: layout({dr: 'h', jc: '<', pd: '1em 0 0 0'})}, cats),
   )
 
-  var cats = curItem.categories.map((v, i) => h(Chip, {key: v,
-    onRequestDelete: ev => modify(null, 'curItem', 'categories', i),
-    style: {margin: '0.25em'},
-  }, (categories.find(e => e.id === v) || {}).title ))
-
   var col2 = h(Theme, {theme: lightTheme},
-    h(Paper, {style: layout({dr: 'v', fx: '1', mg: '0 0 0 3em', pd: '1em'})},
+    h(Paper, {style: layout({dr: 'v', jc: 'a', ai: '+', fx: '1', mg: '0 0 0 3em', pd: '1em'})},
       h('img', {
-        style: {maxWidth: '100%'},
+        style: {maxWidth: '100%', width: 'fit-content'},
         src: curImage.blob instanceof Blob && URL.createObjectURL(curImage.blob)
       })
     )
   )
 
-  return [h('div', {style: layout({dr: 'h'})}, col1, col2),
-    h('div', {style: layout({dr: 'h', jc: '<', pd: '1em 0.75em 0 0.75em'})}, cats)]
+  return [h('div', {style: layout({dr: 'h'})}, col1, col2)]
 }
