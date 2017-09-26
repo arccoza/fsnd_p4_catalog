96c1487745785aed82a7d874afc7ee4fc7ead350
Adrien <adrien@arccoza.com>
2017-09-26 17:54:43 +0900

    Add category edit component

---
+++ src/js/react-app-items-category.js
@@ -0,0 +1,112 @@
+import React from 'react'
+import {lightTheme, darkTheme, Theme} from './react-themes'
+import {TextField, SelectField, MenuItem, Paper, RaisedButton,
+  FlatButton, Chip, CircularProgress,
+  Table, TableBody, TableHeader, TableHeaderColumn, TableRow,
+  TableRowColumn,} from './widgets'
+import {Link} from 'react-router-dom'
+import {layout} from './utils'
+import api from './api.js'
+var h = React.createElement
+var print = console.log.bind(console)
+
+
+function Text({value, children=[]}) {
+  return h('div', {style: {
+    width: '256px',
+    height: '72px',
+    lineHeight: '30px',
+    ...layout({dr: 'v', jc: '+'})}},
+    h('span', null, value, ...children),
+  )
+}
+
+export function Category({items, curCategory, isBusy, setField, modify, mode}) {
+  // print(curItem, curImage)
+  var modeInv = {
+    'edit': 'view',
+    'view': 'edit',
+  }
+
+  var itemRows = items.filter(v => curCategory.items.indexOf(v.id) != -1)
+  .map((v, i) => {
+    return h(TableRow, {key: v},
+      h(TableRowColumn, null,
+        h('img', {src: `/api/files/${v.image}/blob`})
+      ),
+      h(TableRowColumn, null, v.title),
+      h(TableRowColumn, null, v.description),
+    )
+  })
+
+  var col1 =
+  h('div', {style: layout({dr: 'v', 'jc': '<'})},
+    h('h2', null, mode == 'edit' ? 'Edit Category' : 'View Category'),
+    mode != 'edit' ? null : h(RaisedButton, {
+      label: isBusy ? null : 'Save',
+      disabled: isBusy,
+      primary: true,
+      style: {margin: '1em 0 0 0'},
+      onTouchTap: ev => {
+        if (isBusy) return
+        modify({do: 'save', on: 'categories'}, 'action')
+      }
+    }, !isBusy ? null : h(CircularProgress, {size: 15, thickness: 1, className: 'CircularProgress'})),
+    mode != 'edit' ? null : h(FlatButton, {
+      label: isBusy ? null : 'Delete',
+      disabled: isBusy || !curCategory.id,
+      style: {margin: '1em 0 0 0'},
+      onTouchTap: ev => {
+        if (isBusy || !curCategory.id) return
+        modify({do: 'remove', on: 'items'}, 'action')
+      }
+    }, !isBusy ? null : h(CircularProgress, {size: 15, thickness: 1, className: 'CircularProgress'})),
+    !curCategory.id ? null : h(Link, {to: `/${modeInv[mode]}/category/${curCategory.id}`},
+      h(FlatButton, {
+        label: isBusy ? null : mode == 'edit' ? 'view' : 'edit',
+        disabled: isBusy || !curCategory.id,
+        style: {margin: '1em 0 0 0', width: '100%'},
+      }, !isBusy ? null : h(CircularProgress, {size: 15, thickness: 1, className: 'CircularProgress'}))
+    ),
+    h(mode != 'edit' ? Text : TextField, {
+      floatingLabelText: 'Category name',
+      hintText: 'ex: Posters',
+      value: curCategory.title,
+      onChange: setField('curCategory', 'title'),
+    }),
+    h(mode != 'edit' ? Text : TextField, {
+      floatingLabelText: 'Category description',
+      hintText: 'ex: All the movie posters',
+      value: curCategory.description,
+      onChange: setField('curCategory', 'description'),
+    }),
+  )
+
+  var col2 =
+  h(Theme, {theme: lightTheme},
+    h(Paper, {style: layout({dr: 'v', jc: 'b', ai: '<', fx: '1', mg: '0 0 0 3em', pd: '1em'})},
+      h(Table, {
+        style: {maxWidth: '100%', width: '100%'},
+        selectable: false,
+      },
+        h(TableHeader, null,
+          h(TableRow, null,
+            h(TableHeaderColumn, null, 'Image'),
+            h(TableHeaderColumn, null, 'Title'),
+            h(TableHeaderColumn, null, 'Description'),
+          )
+        ),
+        h(TableBody, null,
+          // h(TableRow, null,
+          //   h(TableRowColumn, null, 'blah'),
+          //   h(TableRowColumn, null, 'blah'),
+          //   h(TableRowColumn, null, 'blah'),
+          // )
+          ...itemRows
+        )
+      )
+    )
+  )
+
+  return h('div', {style: layout({dr: 'h'})}, col1, col2)
+}
